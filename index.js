88c41d8b08b2779909e11482b6ad631ea3531555
shivakrishnacherala <151620090+shivakrishnacherala@users.noreply.github.com>
2023-11-21 14:47:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,87 @@
+function formatDate(inputDate) {
+    const [day, month, year] = inputDate.split('/');
+    const date = new Date(year, month - 1, day);
+
+    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    return formattedDate;
+}
+
+function restrict() {
+    var dateInput = document.getElementById("dob");
+    var selectedDate = new Date(dateInput.value);
+
+    var maxD = new Date(dateInput.max);
+    var minD = new Date(dateInput.min);
+
+    if (selectedDate < minD || selectedDate > maxD) {
+        alert("Age should be between " + formatDate(dateInput.min) + " and " + formatDate(dateInput.max));
+    }
+}
+
+function initialState() {
+    var dateInput = document.getElementById("dob");
+    var today = new Date();
+    var maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+    var minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
+
+    dateInput.setAttribute("min", formatDate(minDate.toLocaleDateString()));
+    dateInput.setAttribute("max", formatDate(maxDate.toLocaleDateString()));
+}
+
+window.onload = initialState;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const registrationForm = document.getElementById("registrationForm");
+    const userTableBody = document.getElementById("userTableBody");
+
+    loadUserEntries();
+
+    registrationForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = getValue("name");
+        const email = getValue("email");
+        const pwd = getValue("password");
+        const terms = document.getElementById("terms").checked;
+        const dob = getValue("dob");
+
+        const newRow = userTableBody.insertRow();
+        newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${pwd}</td><td>${dob}</td><td>${terms}</td>`;
+        clearFormFields();
+
+        saveUserEntry(name, email, pwd, dob, terms);
+    });
+
+    function getValue(id) {
+        return document.getElementById(id).value;
+    }
+
+    function clearFormFields() {
+        registrationForm.reset();
+    }
+
+    function loadUserEntries() {
+        const userEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+        clearTable(); // Clear existing entries
+
+        userEntries.forEach(({ name, email, pwd, dob, terms }) => {
+            const newRow = userTableBody.insertRow();
+            newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${pwd}</td><td>${dob}</td><td>${terms}</td>`;
+        });
+    }
+
+    function clearTable() {
+        const userTableBody = document.getElementById("userTableBody");
+        while (userTableBody.firstChild) {
+            userTableBody.removeChild(userTableBody.firstChild);
+        }
+    }
+
+    function saveUserEntry(name, email, pwd, dob, terms) {
+        const userEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+        userEntries.push({ name, email, pwd, dob, terms });
+        localStorage.setItem("userEntries", JSON.stringify(userEntries));
+
+        loadUserEntries();
+    }
+});
